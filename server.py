02ae331b3334fd4854f1787c1dbf08d5b1e8d4d5
Bhseong77 <baekhoon@enjet.co.kr>
2026-05-18 10:34:22 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -11,7 +11,22 @@ import io
 import re
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {
+    "origins": "*",
+    "methods": ["GET", "POST", "OPTIONS"],
+    "allow_headers": ["Content-Type", "Authorization"]
+}})
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
+@app.route("/api/<path:path>", methods=["OPTIONS"])
+def handle_options(path):
+    return "", 204
 
 IMAP_SERVER = os.environ.get("IMAP_SERVER", "gw.enjet.co.kr")
 IMAP_PORT = int(os.environ.get("IMAP_PORT", 993))
